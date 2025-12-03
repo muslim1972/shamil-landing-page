21e3212e8150c724fcc9154afab067099fcb12b1
muslim1972 <muslimakkeel@gmail.com>
2025-12-03 18:14:31 +0300

    Added images to public/images and updated web page code

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // تعيين المسار الأساسي (Base Path) كما فعلنا سابقاً
-  base: "/shamil-landing-page/", 
-
-  // **التعديل الجديد:** توجيه ناتج البناء إلى مجلد 'docs'
-  build: {
-    outDir: 'docs'
-  },
-
   plugins: [react()],
 })
