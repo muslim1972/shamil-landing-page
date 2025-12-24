3a1fbacd424177bf17c1f2bd328e43c4f6b89b18
muslim1972 <muslimakkeel@gmail.com>
2025-12-25 01:54:51 +0300

    تجريبي لرفع الدومين shamelapp.com

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // تم تغيير المسار الأساسي ليتطابق مع اسم المستودع ليعمل المسار المطلق /images/... بشكل صحيح
-  base: '/shamil-landing-page/',  
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'docs',
